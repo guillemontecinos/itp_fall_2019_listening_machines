6a2009c2454ebbf3d8e46832dbc67614ebc1af82
gamontec <gamontec@uc.cl>
2019-09-20 11:52:03 -0400

    assignment 2

---
+++ class_2/homework-2/src/ofApp.h
@@ -0,0 +1,66 @@
+#pragma once
+
+#include "ofMain.h"
+#include "ofxGui.h"
+#include "ofxMLTK.h"
+
+class ofApp : public ofBaseApp {
+public:
+    bool showGui = true;
+    bool showBands = true;
+    bool showCoefs = true;
+    
+    int numberOfOutputChannels = 0;
+    int numberOfInputChannels = 2;
+    int sampleRate = 44100;
+    int frameSize = 1024;
+    int numberOfBuffers = 4;
+    
+//    ofColor backgroundColor;
+    ofMutex mutex;
+    MLTK mltk;
+    
+    ofxPanel gui;
+    
+    ofParameter<bool> showFps;
+//    ofParameter<ofColor> graphColor;
+    ofParameter<float> coefsNoveltyFactor;
+    ofParameter<int> coefNumber;
+    ofxToggle displayScope;
+    
+    vector<vector<float>> coefsBuffer;
+    vector<float> coefsDev;
+    vector<vector<float>> coefsAvg;
+    
+    ofSoundStream soundStream;
+    
+    void audioIn(ofSoundBuffer &inBuffer);
+    
+    // These are the built-ins
+    void setup();
+    void update();
+    void draw();
+    
+    void keyPressed(int key);
+    void keyReleased(int key);
+    void mouseMoved(int x, int y );
+    void mouseDragged(int x, int y, int button);
+    void mousePressed(int x, int y, int button);
+    void mouseReleased(int x, int y, int button);
+    void mouseEntered(int x, int y);
+    void mouseExited(int x, int y);
+    void windowResized(int w, int h);
+    void dragEvent(ofDragInfo dragInfo);
+    void gotMessage(ofMessage msg);
+    
+    ofEasyCam cam;
+    
+    //=============== Mesh Stuff ===============
+    ofVec3f base, vec1, vec2, vec3, vecOut, rotVec1, rotVec2, rotVec3;
+    ofQuaternion qRot1, qRot2, qRot3;
+    ofMesh baseMesh, drawMesh;
+    vector<ofVec3f> offsets;
+    float scale;
+    //=============== Mesh Stuff ===============
+    
+};
--- class_notes.md
+++ class_notes.md
@@ -4,3 +4,11 @@
 * PCM: Pulse-code modulation
 * [Essentia](https://essentia.upf.edu/documentation/documentation.html)
 * threading in coding
+
+## Class 2: Signal processing (kind of)
+In a signal processing pipeline some filters are applied
+* Frame cutter: cuts a frame
+* Windowing: normalizes the frame to get rid of pops at the ends
+* Spectrum: calculates the FFT
+* pools in essentia are databases
+* Pools are [Circular Buffers](https://en.wikipedia.org/wiki/Circular_buffer)
